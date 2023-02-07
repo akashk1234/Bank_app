ce9b44f143ca2a1564449644c6b4f390814cb033
AKASH.K <akashmdy000@gmail.com>
2023-02-07 16:17:49 +0530

    dashboard and register pages added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -9,56 +11,30 @@ export class LoginComponent {
 
   
 
-  inputplaceholder="Enter your Account Number"
+  inputplaceholder="Account Number"
 
-  // acno=""     //or  acno:any
-  // pswd=""
+  acno=''     //or  acno:any
+  psw=''
 
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-    1002:{acno:1002,username:"arun",password:"abc123",balance:0},
-    1003:{acno:1003,username:"akhil",password:"abc123",balance:0},
-  }
+  
 
-  constructor() { }
+  constructor(private router:Router, private ds:DataService) { }
 
   ngOnInit(): void{
 
   }
 
-  // login(){
-  //   var acnum=this.acno
-  //   var psw=this.pswd
-  //   var userDetails=this.userDetails
-    
-  //   if(acnum in userDetails){
-    
-      
-  //     if(psw==userDetails[acnum]["password"]){
-  //       alert("Login Success")
-  //     }
-  //     else{
-  //       alert("Incorrect Password")
-  //     }
-  //   }
-  //   else{
-  //     alert("Incorrect account number or not registered")
-  //   }
-  // }
-
-
-  login(a:any,b:any){
-    
-    var acnum=a.value
-    var psw=b.value
-    var userDetails=this.userDetails
+  login(){
+    var acnum=this.acno
+    var psw=this.psw
+    var userDetails=this.ds.userDetails
     
     if(acnum in userDetails){
     
       
       if(psw==userDetails[acnum]["password"]){
         alert("Login Success")
+        this.router.navigateByUrl('dashboard')
       }
       else{
         alert("Incorrect Password")
@@ -68,6 +44,8 @@ export class LoginComponent {
       alert("Incorrect account number or not registered")
     }
   }
+
+
 
 
 
